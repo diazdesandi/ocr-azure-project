24cee9dff5ae00ce9b1112d41986c77ffe4a84a3
René <diaz.de.sandi.7@gmail.com>
2024-09-25 19:54:17 -0700

    feat: Add POST /ocr endpoint to accept form-data

--- ocr-backend/main.go
+++ ocr-backend/main.go
@@ -6,23 +6,45 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
 type Response struct {
-	ID	  string `json:"id"`
-	Text  string `json:"text"`
+	Ok 	bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+type Image struct {
+	File string `json:"file"`
 }
 
 func main() {
-	router:= gin.Default()
-	router.GET("/ocr", GetOcrText )
+	router := gin.Default()
+	router.GET("/", checkStatus)
+	router.POST("/ocr", postImage)
 	router.Run(":8080")
 }
 
-func GetOcrText(c *gin.Context) {
+func checkStatus(c *gin.Context) {
 	response := Response{
-		ID: "1",
-		Text: "Hello World",
+		Ok: true,
+		Message: "All up and running!",
 	}
 	c.JSON(http.StatusOK, response)
 }
 
+func postImage(c *gin.Context) {
+
+	file, err := c.FormFile("file")
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No file uploaded"})
+		return
+	}
+
+	if err := c.SaveUploadedFile(file, "image.jpg"); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Error saving file"})
+		return
+	}
+
+	response := c.PostForm("response")
+
+	c.JSON(http.StatusOK, gin.H{"file": file.Filename, "response": response})
+}
